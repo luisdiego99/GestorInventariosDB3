5e6c72dc5946705102eb762e6a24cb2dbd749290
GianlucaHernandez <158513807+GianlucaHernandez@users.noreply.github.com>
2025-03-15 22:18:50 -0600

    Update main.sql agregando funciones

--- main.sql
+++ main.sql
@@ -147,6 +147,48 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- create functions
+-- function: This function returns the current stock of a specific product.
+DELIMITER $$
+CREATE FUNCTION get_current_stock(prod_id INT) RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE stock_actual INT;
+    SELECT stock INTO stock_actual
+    FROM products
+    WHERE id_product = prod_id;
+    RETURN stock_actual;
+END $$  
+DELIMITER ;
+
+-- function: This function helps to know how much a product has generated in sales.
+DELIMITER $$
+CREATE FUNCTION get_total_revenue(prod_id INT) RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    DECLARE total DECIMAL(10,2);
+    SELECT SUM(subtotal) INTO total 
+    FROM transactions 
+    WHERE id_product = prod_id AND type_transaction = 'Venta';
+    RETURN IFNULL(total, 0);
+END $$
+DELIMITER ;
+
+-- function: This feature allows you to know how much a specific store has sold.
+DELIMITER $$
+CREATE FUNCTION get_total_sales_by_store(store_id INT) RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    DECLARE total_sales DECIMAL(10,2);
+    SELECT SUM(subtotal) INTO total_sales
+    FROM transactions t
+    JOIN products p ON t.id_product = p.id_product
+    WHERE p.id_store = store_id AND t.type_transaction = 'Venta';
+    RETURN IFNULL(total_sales, 0);
+END $$
+DELIMITER ;
+
+
 -- 5. Populate Initial Data
 -- Insert initial records into stores
 INSERT INTO stores (name_store, max_storage, location) VALUES
@@ -207,6 +249,11 @@ CALL calculate_subtotal(5);
 CALL recalc_product_stock(1);
 CALL recalc_product_stock(3);
 SELECT * FROM products WHERE id_product = 3;
+
+--8 Test functions
+SELECT get_current_stock(2);
+SELECT get_total_revenue(4);
+SELECT get_total_sales_by_store(2);
 
 
 CALL get_sales_report('2023-10-01', '2023-10-31');
